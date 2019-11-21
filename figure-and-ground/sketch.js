5e2e5b526c29a25f1a4f02121d2a5ee3e0642a80
lazio <diniovskiy@gmail.com>
2019-11-21 10:33:07 +0200

    firgure and ground: tall

--- figure-and-ground/sketch.js
+++ figure-and-ground/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // background(255, 204, 0);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  const margin = 50;
+
+  const width = windowWidth / 30;
+  const height = windowHeight - margin;
+
+  const x = windowWidth - width - margin;
+  const y = windowHeight - height + margin;
+
+  fill(0);
+  // translate(x, y);
+  // rotate(radians(20));
+  rect(x, y, width, height);
 }
